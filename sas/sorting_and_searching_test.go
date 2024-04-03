c4e8cc3ab8ffb672ec79f01727b494df9ec2a193
Lisogubenko Vitaliy <vlisogubenko@wimark.com>
2024-04-03 15:55:37 +0400

    [sas] fix merge test

--- sas/sorting_and_searching_test.go
+++ sas/sorting_and_searching_test.go
@@ -12,7 +12,7 @@ func TestMerge(t *testing.T) {
 	}{
 		{nums1: []int{1, 2, 3, 0, 0, 0}, nums2: []int{2, 5, 6}, m: 3, n: 3, out: []int{1, 2, 2, 3, 5, 6}},
 		{nums1: []int{1}, nums2: []int{}, m: 1, n: 0, out: []int{1}},
-		{nums1: []int{}, nums2: []int{1}, m: 0, n: 1, out: []int{1}},
+		{nums1: []int{0}, nums2: []int{1}, m: 0, n: 1, out: []int{1}},
 	}
 
 	for i, test := range testCases {
